e08777a28b76c09d5d5123de50e2a98d4f3204b5
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-02-24 17:26:05 -0300

    docs: creating strucutre form

--- src/components/FormNew/index.js
+++ src/components/FormNew/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Field from '../Field';
-import Form from '../Form';
+import { useForm } from 'react-hook-form';
 
-const FormNew = () => (
-    <Form>
-        <Field.Text name="email" label="Email" type="email" />
-    </Form>
-);
+import Form from '../Form';
+import Field from '../Field';
+import Button from '../Button';
+
+const FormNew = () => {
+    const { register, handleSubmit} = useForm();
+    
+    const newUser = (user) => {
+        console.log(user);
+    }; 
+
+    return (
+        <Form onSubmit={handleSubmit(newUser)}>
+            <Field.Text name="Nome" label="Nome" type="text" ref={register}/>
+            <Field.Text name="email" label="Email" type="email" ref={register}/>
+            <Button>ENVIAR</Button>
+        </Form>        
+    );
+};
 
 
 export default FormNew;
