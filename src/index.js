ec62dff33bed60db81bfd43a53aba3171a0f010b
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-02-24 14:39:21 -0300

    docs: import Base elements (add font)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 import Reset from './styles/generic/Reset'
+import Base from './styles/elements/Base';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Reset/>
+    <Base />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
