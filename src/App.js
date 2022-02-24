58e7616bfab6c0237f2bdddc796e60de8debbfcf
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-02-24 14:40:57 -0300

    docs: create input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Form from './components/Form';
+
 import Main from './components/Main';
+import Form from './components/Form';
+import Field from './components/Field';
 
 function App() {
 
   return(
 
     <Main>
-      <Form></Form>
+      <Form>
+        <Field.Text label="email" type="email" name="email" /> 
+      </Form>
     </Main>
  
  ) ;
